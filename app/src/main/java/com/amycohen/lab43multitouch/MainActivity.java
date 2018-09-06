202cc9d282af2005b64a3ed737460b0607609c3b
Amy Cohen <anbcohen@gmail.com>
2018-09-06 07:27:51 -0700

    instead of a tree like lab 42, added a simplified version and implemented the method necessary

--- app/src/main/java/com/amycohen/lab43multitouch/MainActivity.java
+++ app/src/main/java/com/amycohen/lab43multitouch/MainActivity.java
@@ -2,12 +2,36 @@ package com.amycohen.lab43multitouch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnTouchListener,
+        View.OnLayoutChangeListener{
+
+    @BindView(R.id.image) public ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        image.addOnLayoutChangeListener(this);
+    }
+
+    @Override
+    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+        
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
     }
 }
