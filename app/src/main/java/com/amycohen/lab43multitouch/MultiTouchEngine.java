479cf57d085906df0cc56419abcf85540bd80ada
Amy Cohen <anbcohen@gmail.com>
2018-09-06 07:24:40 -0700

    added butterknife to gradle and started with some simple comments and adjusting the layout to be fully imageview

--- app/src/main/java/com/amycohen/lab43multitouch/MultiTouchEngine.java
+++ app/src/main/java/com/amycohen/lab43multitouch/MultiTouchEngine.java
@@ -1,4 +1,9 @@
 package com.amycohen.lab43multitouch;
 
 public class MultiTouchEngine {
+
+    /*
+    Keep track of where you are touching on the screen. Separated from Drawer Engine because it doesn't actually draw anything, it just tracks what is drawn.
+     */
+
 }
