d2b8adc99124b8c943ab540a361c026c684d6267
Amy Cohen <anbcohen@gmail.com>
2018-09-06 07:40:32 -0700

    creating structure for the touch events and the main activity

--- app/src/main/java/com/amycohen/lab43multitouch/MainActivity.java
+++ app/src/main/java/com/amycohen/lab43multitouch/MainActivity.java
@@ -1,5 +1,7 @@
 package com.amycohen.lab43multitouch;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity implements
         View.OnLayoutChangeListener{
 
     @BindView(R.id.image) public ImageView image;
+    protected Bitmap bitmap;
+    protected Canvas canvas;
+    protected MultiTouchEngine engine;
+    protected MultiTouchEngineDrawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +32,40 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-        
+    public void onLayoutChange(View view, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+        initBitmpap(view);
+    }
+
+    public void initBitmpap(View view) {
+        int width = view.getWidth();
+        int height = view.getHeight();
+
+        bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        engine = new MultiTouchEngine();
+        drawer = new MultiTouchEngineDrawer(engine);
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        int pointers = motionEvent.getPointerCount();
+
+        int action = motionEvent.getAction();
+        int masked = motionEvent.getActionMasked();
+
+        for (int i = 0; i < pointers; i++) {
+            float xx = motionEvent.getX();
+
+            
+            if (action == MotionEvent.ACTION_DOWN || masked == MotionEvent.ACTION_POINTER_DOWN) {
+
+            } else if (action == MotionEvent.ACTION_UP || masked == MotionEvent.ACTION_POINTER_UP) {
+
+            } else if (action == MotionEvent.ACTION_MOVE) {
+
+            }
+        }
         return false;
     }
 }
--- app/src/main/java/com/amycohen/lab43multitouch/MultiTouchEngineDrawer.java
+++ app/src/main/java/com/amycohen/lab43multitouch/MultiTouchEngineDrawer.java
@@ -1,4 +1,8 @@
 package com.amycohen.lab43multitouch;
 
 public class MultiTouchEngineDrawer {
+
+    public MultiTouchEngineDrawer(MultiTouchEngine engine){
+
+    }
 }
