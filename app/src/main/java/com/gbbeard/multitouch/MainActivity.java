9538d83a4be3fcfcdf47b5aab2abf7668ba45acd
GoldBeardSea <timbusch1@gmail.com>
2018-09-09 03:49:57 -0700

    working on building out view then engine and drawer

--- app/src/main/java/com/gbbeard/multitouch/MainActivity.java
+++ app/src/main/java/com/gbbeard/multitouch/MainActivity.java
@@ -1,13 +1,32 @@
 package com.gbbeard.multitouch;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements
+        View.OnTouchListener,
+        View.OnLayoutChangeListener {
+
+    @BindView(R.id.imageView)
+    public ImageView image;
+    protected Bitmap bitmap;
+    protected Canvas canvas;
+    protected MultiTouchEngine engine;
+    protected MultiTouchEngineDrawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
     }
 }
